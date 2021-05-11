b4b563cac91547c925bc47110c71f978fdb7aa36
baba-sheep <c109156244>
2021-05-11 22:54:24 +0800

    C109156244

---
+++ 3.py
@@ -0,0 +1,25 @@
+a=int(input("請輸入年份:"))
+if (a+6)%12==0 :
+    print("生肖為:"+str("tiger"))
+elif (a+6)%12==1 :
+    print("生肖為:"+str("rabbit"))
+elif (a+6)%12==2 :
+    print("生肖為:"+str("dragon"))
+elif (a+6)%12==3 :
+    print("生肖為:"+str("snake"))
+elif (a+6)%12==4 :
+    print("生肖為:"+str("horse"))
+elif (a+6)%12==5 :
+    print("生肖為:"+str("sheep"))
+elif (a+6)%12==6 :
+    print("生肖為:"+str("monkey"))
+elif (a+6)%12==7 :
+    print("生肖為:"+str("rooster"))
+elif (a+6)%12==8 :
+    print("生肖為:"+str("dog"))
+elif (a+6)%12==9 :
+    print("生肖為:"+str("pig"))
+elif (a+6)%12==10 :
+    print("生肖為:"+str("rat"))
+elif (a+6)%12==11 :
+    print("生肖為:"+str("ox"))
